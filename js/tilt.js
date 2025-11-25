20716b9c3484b2bcd3950b5dd975767234860ec6
will-r-young <153227270+will-r-young@users.noreply.github.com>
2025-11-24 19:19:08 -0800

    Added tilt card page

---
+++ js/tilt.js
@@ -0,0 +1,19 @@
+const tilt_card = document.querySelector('.tilt-card');
+
+document.addEventListener("mousemove", (e) => {
+    rotateElement(e, tilt_card);
+});
+
+function rotateElement(event, element) {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    const middleX = window.innerWidth / 2;
+    const middleY = window.innerHeight / 2;
+
+    const offsetX = ((x - middleX) / middleX) * 45; 
+    const offsetY = ((y - middleY) / middleY) * 45; 
+
+    element.style.setProperty("--rotateX", -offsetY + "deg");
+    element.style.setProperty("--rotateY", offsetX + "deg");
+}
